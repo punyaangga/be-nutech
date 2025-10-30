bef1117c3ec5e52c2505136421cc09a21ad5b299
punyaangga <anggafantiya@gmail.com>
2025-10-30 10:27:40 +0700

    add banner repository

---
+++ src/usecases/bannerUsecase.ts
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from "uuid";
+import { BannerRepository } from "../infrastructure/repositories/bannerRepository.js";
+import type { Banner } from "../domain/entities/bannerEntity.js";
+
+export class BannerUseCase {
+    private bannerRepository: BannerRepository;
+    constructor(bannerRepository?: BannerRepository) {
+        this.bannerRepository = bannerRepository ?? new BannerRepository();
+    }
+    async execute(){
+        const banners: Banner[] = await this.bannerRepository.getAllBanner();
+        return banners;
+    }
+}
