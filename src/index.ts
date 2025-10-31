b52e571aa5581ff2d564be20fb2f714243aefc75
punyaangga <anggafantiya@gmail.com>
2025-10-31 19:50:45 +0700

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import { setupSwagger } from "./config/swagger.js";
 import userRoutes from "./interfaces/routes/member_ship/userRoutes.js";
 import userInfoRoutes from "./interfaces/routes/member_ship/userInfoRoutes.js";
 import bannerRoutes from "./interfaces/routes/information/bannerRoutes.js";
 import balanceRoutes from "./interfaces/routes/transaction/balanceRoutes.js";
-import transactionRoutes from "./interfaces/routes/transaction/transactionroutes.js";
+import transactionRoutes from "./interfaces/routes/transaction/transactionRoutes.js";
 import ppobRoutes from "./interfaces/routes/information/ppobRoutes.js";
 import fileUpload from "express-fileupload";
 import { userInfo } from "os";
@@ -12,6 +13,8 @@ import { userInfo } from "os";
 dotenv.config();
 
 const app = express();
+
+setupSwagger(app);
 app.use(express.json());
 app.use(fileUpload());
 
